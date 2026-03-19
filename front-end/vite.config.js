7d547f5568db1371bb6febf1f561aa130ddeb246
Tadesse <tadiosgb27@gmail.com>
2026-03-19 10:43:20 +0300

    the microform

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,33 +2,48 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import commonjs from 'vite-plugin-commonjs'
-
+import fs from 'fs'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 export default defineConfig({
-  plugins: [
-    vue(),
-    commonjs()
-  ],
+plugins: [
+vue(),
+commonjs(),
+// basicSsl() // 👈 REQUIRED
+],
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+resolve: {
+alias: {
+'@': path.resolve(__dirname, 'src')
+}
+},
+
+server: {
+host: '10.195.49.19', // allow external access
+port: 3000,
+strictPort: true,
+ https: {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
   },
+// https:true,
+allowedHosts: ['donation.wegagen.com', 'localhost']
+},
 
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: 'all'
-  },
+preview: {
+host: '0.0.0.0',
+port: 3000,
+strictPort: true
+},
 
-  preview: {
-    host: '0.0.0.0',
-    port: 3000
-  },
+optimizeDeps: {
+include: ['rtcpeerconnection-shim', 'sdp']
+},
 
-  optimizeDeps: {
-    include: ['rtcpeerconnection-shim', 'sdp']
-  },
-
-  assetsInclude: ['**/*.PNG', '**/*.JPG', '**/*.jpeg', '**/*.gif', '**/*.svg']
+assetsInclude: [
+'**/*.PNG',
+'**/*.JPG',
+'**/*.jpeg',
+'**/*.gif',
+'**/*.svg'
+]
 })
